0f9dcddddd40093cd3acf027ee8a86c13fe7d984
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-10-28 22:18:53 +0300

    otelreservation

---
+++ otel-reservation/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <b-navbar fixed="top" toggleable="lg" type="dark" variant="dark">
+          <b-navbar-brand href="#">
+
+          </b-navbar-brand>
+      </b-navbar>
+      <b-navbar-toggle>
+          
+      </b-navbar-toggle>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
