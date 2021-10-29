308bd39f46d8aa15d8ce3c9aa2d68f8c1146fa17
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-10-29 03:29:57 +0300

    firebase

--- otel-reservation/src/components/Navbar.vue
+++ otel-reservation/src/components/Navbar.vue
@@ -2,21 +2,103 @@
   <div>
       <b-navbar fixed="top" toggleable="lg" type="dark" variant="dark">
           <b-navbar-brand href="#">
-
+              <i class="fas fa-hotel"></i>Otel App
           </b-navbar-brand>
       </b-navbar>
-      <b-navbar-toggle>
+      <b-navbar-toggle id="nav-collapse">
           
       </b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-button>
+                
+              </b-button>
+            </b-nav-form>
+
+          </b-navbar-nav>
+
+        
+
+      </b-collapse>
   </div>
 </template>
 
 <script>
+import {db} from '@/firebase';
+import Swal from 'sweetalert2'
 export default {
-
+  data(){
+    return {
+      form:{
+        reservationid: "",
+        message: "",
+        room: null,
+        email: ''
+      },
+      rooms:[
+        {text:"Select One", value: null},
+        "Single",
+        "Double",
+        "Family",
+        "Deluxe"
+      ],
+      color: "",
+      title: "",
+      show: false,
+      reservation: {
+        id: "",
+        name: "",
+        email: "",
+        day: "",
+        price: ""
+      },
+      admin:{
+        user:"arif",
+        pass: '1234'
+      },
+      admininput:{
+        user: '',
+        pass: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit2(evt){
+      this.show=false;
+      evt.preventDefault();
+      switch(this.title){
+          case 'Cancel Reservation' :
+            db.collection(this.form.room)
+              .doc(this.form.reservationid)
+              .delete()
+              .then(()=>{
+                this.$bvModal.hide('my-modal2')
+                Swal.fire({
+                  icon: 'success',
+                  title: 'Reservation Canceled'
+                });
+              })
+              .catch(()=>{
+                console.log('Error')
+              });
+              break;
+          
+      }
+    }
+  },
 }
 </script>
 
 <style>
-
+i{
+  color: orange;
+}
+button:hover{
+  background-color: orange;
+}
+button:hover > i{
+  color: white;
+}
 </style>
--- otel-reservation/src/views/Home.vue
+++ otel-reservation/src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="home">
-    
+    <Navbar/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'Home',
   components: {
-    
+    Navbar
   }
 }
 </script>
